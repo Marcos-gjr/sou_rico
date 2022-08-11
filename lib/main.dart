57daab2435c24a9df859bee325ce904441d0fe9a
Marcos Jr <m.goncalves.oficial@gmail.com>
2022-08-10 23:38:20 -0300

    Im a Ducky

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ void main() {
       backgroundColor: const Color(0xFF096BDB),
       appBar: AppBar(
         title: const Text('Im a Duck'),
-        backgroundColor: const Color(0xFF0000FF),
+        backgroundColor: const Color(0xFF0F0FFF),
 
       ),
         body: const Center(
