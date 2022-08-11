b236c76ed06ea15493e060bdb7a9990e529d1d98
Marcos Jr <m.goncalves.oficial@gmail.com>
2022-08-10 23:24:30 -0300

    Im a Duck

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,21 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: const Color(0xFF096BDB),
+      appBar: AppBar(
+        title: const Text('Im a Duck'),
+        backgroundColor: const Color(0xFF0000FF),
 
-    
+      ),
+        body: const Center(
+          child: Image(
+            image: AssetImage('images/psyduck.png')
+          ),
+
+        ),
+    ),
+
   ));
 }
 
